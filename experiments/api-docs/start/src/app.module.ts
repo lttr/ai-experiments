3573183012dbdf1177fbf5f780c3fa7c2d6058d6
Lukas Trumm <lukas.trumm@gmail.com>
2025-03-17 16:38:37 +0100

    aider: refactor: Remove default AppController and update AppModule

--- experiments/api-docs/start/src/app.module.ts
+++ experiments/api-docs/start/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [BooksModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
